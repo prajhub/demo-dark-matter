45fc9bca85431804026b17f8675aa31ae9665704
prajwal <pzlmagar@gmail.com>
2023-09-05 17:24:52 +0545

    minor apos

--- components/writenow.tsx
+++ components/writenow.tsx
@@ -4,7 +4,7 @@ export default function WriteNow() {
         <section className="max-w-full bg-white">
         <div className="flex flex-col items-center py-10">
     <div className="text-black flex flex-col py-2 px-4  max-w-[1050px] mx-auto">
-        <h1 className=" text-center md:mt-4 font-bold md:text-5xl text-3xl tracking-wide">Didn't find the perfect fit?</h1>
+        <h1 className=" text-center md:mt-4 font-bold md:text-5xl text-3xl tracking-wide">Didn&apos;t find the perfect fit?</h1>
 
         <p className=" leading-normal text-center md:text-start font-medium md:text-xl text-lg line tracking-wide py-12">No worries! We&apos;re always on the lookout for talented individuals who share our passion for gaming. If you think you have something special to bring to the table, drop us a line at <span className=" underline"> careers@darkmattergames.com </span>and let&apos;s chat about how you can be part of our gaming revolution. Let your passion and talents shine as we embark on an exciting journey together. </p>
         <p className=" leading-normal font-medium text-center md:text-start md:text-xl text-lg md:py-7 line tracking-wide ">Join us and help shape the future of gaming with innovation, imagination, and endless possibilities. Together, let&apos;s create gaming experiences that leave a lasting impact on players worldwide.</p>
